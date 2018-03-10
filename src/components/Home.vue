965fceefed3727c8dba48cf7c2483f0cf923f7f2
Steven Avelino <steven.avelino24@outlook.com>
2018-03-10 13:44:51 +0100

    Adding files.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,71 @@
 <v-content>
   <section>
     <v-parallax :src="require('@/assets/title-hero.jpg')" height="450">
-      <v-layout column align-center justify-center class="white--text">
-        <h1 class="white--text mb-2 display-1 text-xs-center">Steven Avelino</h1>
+      <v-layout column align-center justify-center class="black--text">
+        <h1 class="black--text mb-2 display-1 text-xs-center">Steven Avelino</h1>
         <div class="subheading mb-3 text-xs-center">Technicien en développement d'applications</div>
       </v-layout>
     </v-parallax>
+  </section>
+  <section>
+    <v-layout column wrap align-center class="my-5">
+      <v-flex xs12 sm4 class="my-3">
+        <div class="text-xs-center">
+          <h2 class="headline">Informations personnelles</h2>
+        </div>
+      </v-flex>
+      <v-flex xs12>
+        <v-container grid-list-xl>
+          <v-layout row wrap align-center>
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                <v-card-text class="text-xs-center">
+                  <v-icon x-large class="purple--text text--lighten-2">contacts</v-icon>
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline text-xs-center"></div>
+                </v-card-title>
+                <v-card-text>
+                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                <v-card-text class="text-xs-center">
+                  <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Fast development</div>
+                </v-card-title>
+                <v-card-text>
+                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                <v-card-text class="text-xs-center">
+                  <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline text-xs-center">Completely Open Sourced</div>
+                </v-card-title>
+                <v-card-text>
+                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
   </section>
 </v-content>
 </template>
